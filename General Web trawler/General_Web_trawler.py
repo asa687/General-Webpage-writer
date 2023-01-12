c6efda955a1a490344d0530962c6a94874793466
Sean Abong <sa2278@york.ac.uk>
2023-01-12 00:14:47 +0000

    fixed error with unsupported characters

--- General Web trawler/General_Web_trawler.py
+++ General Web trawler/General_Web_trawler.py
@@ -1,7 +1,6 @@
 """ 
 this is a simple page scraper that retrieves a webpage and stores it into a .txt file
 
-
 """
 
 import re
@@ -9,8 +8,8 @@ from urllib.request import urlopen
 print("enter the desired url: ")
 url = input() 
 webpage = urlopen(url) 
-## the decodes for page are more complex due to certain characters being unavailable in uft-8
-page = (webpage.read()).decode("utf-8").encode('cp850','replace').decode('cp850') 
+## the decodes for page are more complex due to certain characters being unavailable in uft-8, so 
+page = (((webpage.read()).decode("utf-8")).encode('cp850','replace')).decode('cp850') 
 pattern = "<title.*?>.*?</title.*?>"
 if (re.search(pattern,page, re.IGNORECASE) != None): 
     titles = re.search(pattern,page, re.IGNORECASE) 
